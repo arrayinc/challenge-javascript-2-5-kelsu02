5dad2d5093697f4fc00e7c19ba999628677d7fa3
kredmond <kelsu02@gmail.com>
2019-11-21 11:41:57 -0700

    Finished Challenge

--- answer.js
+++ answer.js
@@ -1,1 +1,17 @@
 // *** YOUR ANSWER BELOW ***
+function negate(str) {
+    return "un" + str;
+}
+
+function intensify(str) {
+    return "plus" + str;
+}
+
+function reinforce(str) {
+    return "double" + str;
+}
+
+console.log(negate("cold"));
+console.log(intensify("cold"));
+console.log(reinforce(intensify("cold")));
+console.log(reinforce(intensify(negate("good"))));
